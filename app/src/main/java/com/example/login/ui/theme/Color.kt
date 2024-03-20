74eb1a4c32c59ac9744f9ac5dfc7d2de8fbcda32
Norpha <normanndiritu@gmail.com>
2024-03-20 17:40:50 +0300

    email update

--- app/src/main/java/com/example/login/ui/theme/Color.kt
+++ app/src/main/java/com/example/login/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.example.login.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val equity1 = Color(0xFF992C24)
+val PurpleGrey80 = Color(0xFF992C24)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
